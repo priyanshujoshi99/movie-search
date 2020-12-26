36c1148704953f2e2de0c1bdf7ca485f028bdf45
Priyanshujoshi99 <joshipriyanshu2029@gmail.com>
2020-12-26 18:52:35 +0530

    Updates

---
+++ precache-manifest.6eea671e5c373ff4f1c20108d5e552be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566a9599e125345eca2ccd35487ee78b",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "010fce49bbba028dc973",
+    "url": "/movie-search/static/css/main.97415529.chunk.css"
+  },
+  {
+    "revision": "61988b5cc12264e24f64",
+    "url": "/movie-search/static/js/2.778083eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.778083eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010fce49bbba028dc973",
+    "url": "/movie-search/static/js/main.d02212b8.chunk.js"
+  },
+  {
+    "revision": "8f1314dfdc21c95c9344",
+    "url": "/movie-search/static/js/runtime-main.8345f5b5.js"
+  }
+]);
